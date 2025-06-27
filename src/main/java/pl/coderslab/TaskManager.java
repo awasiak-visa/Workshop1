0176181dd9ca512f86059917a2005066c1b0d345
awasiak-visa <awasiak@visa.com>
2025-06-27 10:57:51 +0200

    Method readFile added

--- src/main/java/pl/coderslab/TaskManager.java
+++ src/main/java/pl/coderslab/TaskManager.java
@@ -1,4 +1,33 @@
 package pl.coderslab;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+
 public class TaskManager {
+
+    public static void main(String[] args) {
+        try {
+            readFile("tasks.csv");
+        } catch (IOException e) {
+            throw new RuntimeException();
+        }
+    }
+
+    public static void readFile (String fileName) throws IOException {
+        Path path = Paths.get(fileName);
+        if (Files.exists(path)) {
+            String fileContent = Files.readString(path);
+            String[] rows = fileContent.split("\n");
+            int columnsNum = rows[0].split(",").length;
+            String[][] tasks = new String[rows.length][columnsNum];
+            for (int i = 0; i < rows.length; i++) {
+                tasks[i] = rows[i].split(",");
+            }
+        } else {
+            System.out.println("File " + fileName + " not found");
+        }
+    }
 }
